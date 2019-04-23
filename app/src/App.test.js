d942380e727fab6306ecd089386447e7162c4a52
Federico Pacheco <federicogpacheco@gmail.com>
2019-04-23 20:21:17 -0300

    added jest, enzyme and a simple test for app component

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { shallow } from 'enzyme';
+import { exportAllDeclaration } from '@babel/types';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('app render initial text', () => {
+  const app = shallow(<App />);
+  
+  expect(app.text()).toEqual("Edit src/App.js and save to reload.Learn React");
+
 });
